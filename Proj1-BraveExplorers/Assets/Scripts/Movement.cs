b325113beaaa0ef4caf19c6971df0a112e32b769
Ana Rita Torres <anaritatorres96@outlook.pt>
2018-10-23 18:54:04 +0100

    não está a fazer nada, mas depois acabo

---
+++ Proj1-BraveExplorers/Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public GameObject character;
+	Rigidbody characterRigidBody; 
+	
+	public float speed = 10f;
+	float directionX;
+
+	void Start () {
+		characterRigidBody = character.AddComponent<Rigidbody>();
+		characterRigidBody.mass = 5;
+	}
+	
+	void Update () {
+		
+	
+		
+		
+	}
+}
