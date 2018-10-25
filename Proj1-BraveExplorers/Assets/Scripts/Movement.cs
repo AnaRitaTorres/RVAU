439f21ae847cc5bfa13a5f49aa7d44c9d599ee01
Rui Soares <rui.pedro.csoares@gmail.com>
2018-10-25 15:51:31 +0100

    Move Left or Right (not while pressed tho)

--- Proj1-BraveExplorers/Assets/Scripts/Movement.cs
+++ Proj1-BraveExplorers/Assets/Scripts/Movement.cs
@@ -6,60 +6,85 @@ using UnityEngine.UI;
 public class Movement : MonoBehaviour {
 
 	public enum JumpStatus {Grounded, GoingUp, GoingDown};
+	public enum MoveStatus {Grounded, MovingLeft, MovingRight};
 
 	public GameObject character;
 	public Button leftButton, rightButton, jumpButton;
-	bool grounded = true;
-	public JumpStatus status = JumpStatus.Grounded;
+	public JumpStatus jstatus = JumpStatus.Grounded;
+	public MoveStatus mstatus = MoveStatus.Grounded;
 
 	float jumpTime = 0.75f;
 	float currTime = 0.0f;
-	float currSpeed = 0.0f;
+	float currJumpSpeed = 0.0f;
+	float currDirectionalSpeed = 0.0f;
 			
 	void Start () {
-		status = JumpStatus.Grounded;
+		jstatus = JumpStatus.Grounded;
+		mstatus = MoveStatus.Grounded;
 		leftButton.onClick.AddListener(MoveLeft);
 		rightButton.onClick.AddListener(MoveRight);
 		jumpButton.onClick.AddListener(Jump);
 	}
 	
 	void MoveLeft() {
-		character.transform.Translate(Vector3.left * Time.deltaTime);
-
+		if (mstatus == MoveStatus.Grounded){ 
+			mstatus = MoveStatus.MovingLeft;
+			currDirectionalSpeed = 0.1f; 
+		}
+		else if (mstatus == MoveStatus.MovingLeft) {
+			mstatus = MoveStatus.Grounded;
+			currDirectionalSpeed = 0.0f;
+		}
 	}
 
 	void MoveRight() {
-		character.transform.Translate(Vector3.right * Time.deltaTime);
+		if (mstatus == MoveStatus.Grounded){ 
+			mstatus = MoveStatus.MovingRight;
+			currDirectionalSpeed = -0.1f; 
+		}
+		else if (mstatus == MoveStatus.MovingRight) {
+			mstatus = MoveStatus.Grounded;
+			currDirectionalSpeed = 0.0f;
+		}
 	}
 
 	void Jump() {
-		if (status != JumpStatus.Grounded) return;
+		if (jstatus != JumpStatus.Grounded) return;
 		else {
-			status = JumpStatus.GoingUp;
+			jstatus = JumpStatus.GoingUp;
 		}
 	}
 
 	void Update() {
-		if (status != JumpStatus.Grounded) {
-			if (status == JumpStatus.GoingUp) {
+
+		// Jumping behaviour
+
+		if (jstatus != JumpStatus.Grounded) {
+			if (jstatus == JumpStatus.GoingUp) {
 				currTime += Time.deltaTime;
-				currSpeed = 0.18f;
-				character.transform.Translate(Vector3.up * currSpeed * Time.deltaTime);
+				currJumpSpeed = 0.18f;
+				character.transform.Translate(Vector3.up * currJumpSpeed * Time.deltaTime);
 			}
 			else {
 				currTime += Time.deltaTime;
-				currSpeed = -0.18f;
-				character.transform.Translate(Vector3.up * currSpeed * Time.deltaTime);
+				currJumpSpeed = -0.18f;
+				character.transform.Translate(Vector3.up * currJumpSpeed * Time.deltaTime);
 			}
 		}
-		if (currTime >= jumpTime / 2 && currTime < jumpTime && status != JumpStatus.GoingDown) {
+		if (currTime >= jumpTime / 2 && currTime < jumpTime && jstatus != JumpStatus.GoingDown) {
 			currTime = jumpTime / 2;
-			status = JumpStatus.GoingDown;
+			jstatus = JumpStatus.GoingDown;
 		}
 		if (currTime >= jumpTime) {
 			currTime = 0.0f;
-			status = JumpStatus.Grounded;
-			currSpeed = 0.0f;
+			jstatus = JumpStatus.Grounded;
+			currJumpSpeed = 0.0f;
 		}
+
+		// Moving Behaviour
+		if (mstatus != MoveStatus.Grounded){
+			character.transform.Translate(Vector3.left * currDirectionalSpeed * Time.deltaTime);
+		}
+
 	}
 }
