ae313e7cd0367e5e121ef269d63767f5c4a128fe
Rui Soares <rui.pedro.csoares@gmail.com>
2018-10-29 16:54:48 +0000

    blegh

---
+++ Proj1-BraveExplorers/Assets/Scripts/LockObjects.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class LockObjects : MonoBehaviour {
+
+	public Button lockButton;
+	public GameObject chaiseLong;
+	public GameObject basketball;
+	public GameObject books;
+	public GameObject camera;
+	
+	void Start () {
+		lockButton.onClick.AddListener(LockInPlace);	
+	}
+	
+	void LockInPlace(){
+
+		// This makes it so there are many chaise longues... We might wanna change this
+		// TODO
+		var newChaise = Instantiate(chaiseLong, chaiseLong.transform.position, chaiseLong.transform.rotation);
+		newChaise.transform.parent = camera.transform;
+		newChaise.transform.position = new Vector3(newChaise.transform.position.x, newChaise.transform.position.y, 0.0f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
