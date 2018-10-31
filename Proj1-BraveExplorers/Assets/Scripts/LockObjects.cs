971fbf4935dcf4551614b1b73b6fb6d0fe169832
Ana Rita Torres <anaritatorres96@outlook.pt>
2018-10-31 15:59:42 +0000

    colliders and jump is a work

--- Proj1-BraveExplorers/Assets/Scripts/LockObjects.cs
+++ Proj1-BraveExplorers/Assets/Scripts/LockObjects.cs
@@ -10,7 +10,7 @@ public class LockObjects : MonoBehaviour {
 	public GameObject chaiseLong;
 	public GameObject basketball;
 	public GameObject books;
-	public GameObject camera;
+	public Camera secondCamera;
 	//public GameObject blue;
 	
 	void Start () {
@@ -22,10 +22,9 @@ public class LockObjects : MonoBehaviour {
 		// This makes it so there are many chaise longues... We might wanna change this
 		// TODO
 		var newChaise = Instantiate(chaiseLong, chaiseLong.transform.position, chaiseLong.transform.rotation);
-		newChaise.transform.parent = camera.transform;
-		newChaise.transform.localPosition = new Vector3(newChaise.transform.position.x, -newChaise.transform.position.y, 0.0f);
+		newChaise.transform.parent = secondCamera.transform;
 		newChaise.transform.localScale =  new Vector3(5, 5, 5);
-		
+		newChaise.transform.localPosition = new Vector3(newChaise.transform.position.x, -newChaise.transform.position.y, 0.0f);
 		
 	}
 
