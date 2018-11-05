737a51026cfa3dcdf625d138697f4ffe3fbba84f
Rui Soares <rui.pedro.csoares@gmail.com>
2018-11-05 23:20:28 +0000

    Start Game

---
+++ Proj1-BraveExplorers/Assets/Scripts/Play.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Play : MonoBehaviour {
+
+	public enum Status {Placing, Playing};
+	public Button startButton;
+	public Status status;
+
+	public GameObject readyButton;
+	public GameObject leftButton;
+	public GameObject rightButton;
+	public GameObject pauseButton;
+	public GameObject jumpButton;
+	public GameObject undoButton;
+	public GameObject lockButton;
+	public GameObject blue;
+	public GameObject timer;
+
+	// Use this for initialization
+	void Start () {
+		status = Status.Placing;
+		startButton.onClick.AddListener(StartGame);
+
+		leftButton.SetActive(false);
+		rightButton.SetActive(false);
+		jumpButton.SetActive(false);
+		pauseButton.SetActive(false);
+		blue.SetActive(false);
+		timer.SetActive(false);
+
+	}
+
+	public void StartGame(){
+		this.status = Status.Playing;
+
+		leftButton.SetActive(true);
+		rightButton.SetActive(true);
+		jumpButton.SetActive(true);
+		pauseButton.SetActive(true);
+		blue.SetActive(true);
+		timer.SetActive(true);
+
+		undoButton.SetActive(false);
+		readyButton.SetActive(false);
+		lockButton.SetActive(false);
+		
+	}
+}
