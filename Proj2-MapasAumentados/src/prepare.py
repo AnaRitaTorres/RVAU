eb42bd404a32aa1a394f156dbc8652015fb77ce5
Cláudia Marinho <claudia.m.r.marinho@gmail.com>
2018-12-27 19:16:51 +0000

    pickling working for all images & added test logs

--- Proj2-MapasAumentados/src/prepare.py
+++ Proj2-MapasAumentados/src/prepare.py
@@ -27,13 +27,14 @@ def runSIFT(map, test):
     pointsimg = drawKeypoints(grayscaled, keypoints, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     # Save Keypoints
-    save_keypoints(keypoints, descriptors)
+    save_keypoints(trimmed_map_name, keypoints, descriptors, test)
+
     # Show image and wait
-    if (test):
+    if test:
         print("Calculated " + str(len(keypoints)) + " keypoints in " + map)
 
     # Load Keypoints
-    load_keypoints()
+    load_keypoints(trimmed_map_name, test)
 
     return pointsimg
 
@@ -48,8 +49,8 @@ args = parser.parse_args()
 print(os.listdir('../img'))
 
 # If no map is given in command, quit
-if (args.map == None):
-    if (args.test):
+if args.map is None:
+    if args.test:
         print('No map loaded! Quitting...')
     quit()
 
@@ -64,9 +65,9 @@ trimmed_map_name = args.map.split('.')[0]
 # For every map in the img folder, match with the map to open and run SIFT
 for m in os.listdir('..\\img\\'):
     # match map
-    if (re.match("" + trimmed_map_name + r"(\w)*\.\w\w\w", m)):
+    if re.match("" + trimmed_map_name + r"(\w)*\.\w\w\w", m):
         # Run SIFT on map image
-        img = runSIFT("..\\\img\\" + m, args.test)
+        img = runSIFT("..\\img\\" + m, args.test)
 
         # Show Map
         namedWindow('Keypoints', cv2.WINDOW_NORMAL)
@@ -75,4 +76,3 @@ for m in os.listdir('..\\img\\'):
         # Wait for Key Inputs
         cv2.waitKey(0)
         destroyAllWindows()
-
--- Proj2-MapasAumentados/src/utils.py
+++ Proj2-MapasAumentados/src/utils.py
@@ -1,22 +1,29 @@
 from cv2 import *
 import numpy as np
 import pickle
+import os
 
-filename = 'map.bin'
+dir_path = os.path.dirname(os.path.realpath(__file__))
+database_path = dir_path + '/database/'
 
-def save_keypoints(keypoints, descriptors):
+
+# Save Keypoints to file
+def save_keypoints(filename, keypoints, descriptors, test):
+    if not os.path.exists(database_path):
+        os.makedirs(database_path)
+
     kp = serialize_keypoints(keypoints, descriptors)
 
-    binary_file = open(filename, mode='wb')
+    if test:
+        print('\nSaving keypoints and their descriptors to file ' + filename + '_kp.bin\n');
+
+    binary_file = open(database_path + filename + '_kp.bin', mode='wb')
     pickle.dump(kp, binary_file)
     binary_file.close()
 
 
-# Serializes Keypoints and their Descriptors to save them in map.bin
+# Serializes Keypoints and their Descriptors to save them
 def serialize_keypoints(keypoints_list, descriptors_list):
-    print(keypoints_list[0].pt)
-    print(descriptors_list[0])
-
     keypoints = []
     # Get Information From Each Keypoint and store it in keypoints array
     for point, desc in zip(keypoints_list, descriptors_list):
@@ -28,13 +35,13 @@ def serialize_keypoints(keypoints_list, descriptors_list):
 
 
 # Load Keypoints From File
-def load_keypoints():
-    infile = open(filename, 'rb')
+def load_keypoints(filename, test):
+    infile = open(database_path + filename + '_kp.bin', 'rb')
     loaded = pickle.load(infile)
     results = deserialize_keypoints(loaded)
 
-    print(results['k'][0].pt)
-    print(results['d'][0])
+    if test:
+        print('\nLoaded keypoints and their descriptors from file ' + filename + '_kp.bin');
 
     infile.close()
 
@@ -45,12 +52,11 @@ def deserialize_keypoints(keypoints):
     descriptors = []
 
     for point in keypoints:
-        temp_feature = cv2.KeyPoint(x=point[0][0], y=point[0][1], _size=point[1], _angle=point[2], _response=point[3],
-                                    _octave=point[4], _class_id=point[5])
-        temp_descriptor = point[6]
+        feature = cv2.KeyPoint(x=point[0][0], y=point[0][1], _size=point[1], _angle=point[2], _response=point[3], _octave=point[4], _class_id=point[5])
+        descriptor = point[6]
 
-        features.append(temp_feature)
-        descriptors.append(temp_descriptor)
+        features.append(feature)
+        descriptors.append(descriptor)
 
     descriptors = np.asarray(descriptors)
 
