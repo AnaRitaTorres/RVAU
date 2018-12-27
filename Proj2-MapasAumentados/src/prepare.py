850d98233ba9688c2d44c3ddcfb75187299538c2
Cláudia Marinho <claudia.m.r.marinho@gmail.com>
2018-12-27 16:07:30 +0000

    serializing and deserializing keypoints

--- Proj2-MapasAumentados/src/prepare.py
+++ Proj2-MapasAumentados/src/prepare.py
@@ -9,6 +9,9 @@ from cv2 import *
 import argparse
 import numpy as np
 
+from utils import *
+
+
 # A function to run the SIFT algorithm in the map image(s)
 def runSIFT(map):
     # Read map image and convert it to grayscale
@@ -17,28 +20,42 @@ def runSIFT(map):
 
     # Run the SIFT algorithm to detect keypoints
     sift = cv2.xfeatures2d.SIFT_create()
-    keypoints = sift.detect(grayscaled, None)
+    keypoints, descriptors = sift.detectAndCompute(grayscaled, None)
 
     # Draw Keypoints in the image
     pointsimg = drawKeypoints(grayscaled, keypoints, img)
 
-    # Show image and wait
-    imshow('Keypoints', img)
-    waitKey(5000)
+    # Save Keypoints
+    save_keypoints(keypoints, descriptors)
+
+    # Load Keypoints
+    load_keypoints()
+
+    return pointsimg
 
 # Load up parameters
 # python prepare.py -m {mapfile} [-t | --test]
 parser = argparse.ArgumentParser(description="Sets up a map and its points of interest")
 parser.add_argument('-m', '--map', dest='map', default=None, type=str)
-parser.add_argument('-t', '--test', dest='test', action = 'store_true')
+parser.add_argument('-t', '--test', dest='test', action='store_true')
 
 args = parser.parse_args()
 
 # if test flag detected
-if (args.test):
+if args.test:
     print('Starting in Test Mode!')
     print('Loading: ' + args.map)
-    runSIFT(args.map)
-if (args.map == None):
+
+    # Run SIFT on map image
+    img = runSIFT(args.map)
+
+    # Show Map
+    namedWindow('Keypoints', cv2.WINDOW_NORMAL)
+    imshow('Keypoints', img)
+
+    # Wait for Key Inputs
+    cv2.waitKey(0)
+    destroyAllWindows()
+if args.map is None:
     print('No map loaded! Quitting...')
     quit()
---
+++ Proj2-MapasAumentados/src/utils.py
@@ -0,0 +1,58 @@
+from cv2 import *
+import numpy as np
+import pickle
+
+filename = 'map.bin'
+
+
+def save_keypoints(keypoints, descriptors):
+    kp = serialize_keypoints(keypoints, descriptors)
+
+    binary_file = open(filename, mode='wb')
+    pickle.dump(kp, binary_file)
+    binary_file.close()
+
+
+# Serializes Keypoints and their Descriptors to save them in map.bin
+def serialize_keypoints(keypoints_list, descriptors_list):
+    print(keypoints_list[0].pt)
+    print(descriptors_list[0])
+
+    keypoints = []
+    # Get Information From Each Keypoint and store it in keypoints array
+    for point, desc in zip(keypoints_list, descriptors_list):
+        kp = (point.pt, point.size, point.angle, point.response, point.octave, point.class_id, desc)
+        keypoints.append(kp)
+
+    keypoints = np.asarray(keypoints)
+    return keypoints
+
+
+# Load Keypoints From File
+def load_keypoints():
+    infile = open(filename, 'rb')
+    loaded = pickle.load(infile)
+    results = deserialize_keypoints(loaded)
+
+    print(results['k'][0].pt)
+    print(results['d'][0])
+
+    infile.close()
+
+
+# Deserialize Keypoints and their Descriptors
+def deserialize_keypoints(keypoints):
+    features = []
+    descriptors = []
+
+    for point in keypoints:
+        temp_feature = cv2.KeyPoint(x=point[0][0], y=point[0][1], _size=point[1], _angle=point[2], _response=point[3],
+                                    _octave=point[4], _class_id=point[5])
+        temp_descriptor = point[6]
+
+        features.append(temp_feature)
+        descriptors.append(temp_descriptor)
+
+    descriptors = np.asarray(descriptors)
+
+    return {'k': features, 'd': descriptors}
